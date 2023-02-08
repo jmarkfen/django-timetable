dc085fc52f50a5cfbb66ad2948e03e8cf34ec35d
jmrkfnz <119049235+jmrkfnz@users.noreply.github.com>
2023-02-08 11:20:39 +0800

    Fix models.py

--- core/models.py
+++ core/models.py
@@ -35,7 +35,7 @@ class Section(models.Model):
         verbose_name_plural = _("sections")
 
     def __str__(self):
-        return self.year_level + self.section
+        return str(self.year_level) + self.section
 
     def get_absolute_url(self):
         return reverse("section_detail", kwargs={"pk": self.pk})
@@ -43,8 +43,8 @@ class Section(models.Model):
 
 class Subject(models.Model):
 
-    subject_code = models.CharField(_(""), max_length=50)
-    subject_description = models.CharField(_(""), max_length=200)
+    subject_code = models.CharField(max_length=50)
+    subject_description = models.CharField(max_length=200)
 
     class Meta:
         verbose_name = _("subject")
