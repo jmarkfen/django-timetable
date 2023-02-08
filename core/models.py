12ea14ab83ab933f58be05fc71db0ccd59a14e13
jmrkfnz <119049235+jmrkfnz@users.noreply.github.com>
2023-02-08 11:01:29 +0800

    Update models.py

--- core/models.py
+++ core/models.py
@@ -4,7 +4,9 @@ from django.db import models
 
 class Professor(models.Model):
 
-    
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    middle_initial = models.CharField(max_length=50)
 
     class Meta:
         verbose_name = _("professor")
@@ -16,17 +18,22 @@ class Professor(models.Model):
     def get_absolute_url(self):
         return reverse("professor_detail", kwargs={"pk": self.pk})
 
+    @property
+    def full_name_last_name_first(self):
+        return self.last_name + ', ' + self.first_name + ' ' + self.middle_initial + '.'
+
 
 class Section(models.Model):
 
-    
+    year_level = models.PositiveSmallIntegerField()
+    section = models.CharField(max_length=2)
 
     class Meta:
         verbose_name = _("section")
         verbose_name_plural = _("sections")
 
     def __str__(self):
-        return self.name
+        return self.year_level + self.section
 
     def get_absolute_url(self):
         return reverse("section_detail", kwargs={"pk": self.pk})
@@ -34,7 +41,8 @@ class Section(models.Model):
 
 class Subject(models.Model):
 
-    
+    subject_code = models.CharField(_(""), max_length=50)
+    subject_description = models.CharField(_(""), max_length=200)
 
     class Meta:
         verbose_name = _("subject")
@@ -49,7 +57,26 @@ class Subject(models.Model):
 
 class Schedule(models.Model):
 
-    
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    SESSION_TYPE_CHOICES = [
+        ('Lec', 'Lecture'),
+        ('Lab', 'Laboratory'),
+    ]
+    session_type = models.CharField(max_length=50, choices=SESSION_TYPE_CHOICES)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    DAYS_OF_WEEK = [
+        ('M', 'Monday'),
+        ('T', 'Tuesday'),
+        ('W', 'Wednesday'),
+        ('TH', 'Thursday'),
+        ('F', 'Friday'),
+        ('SA', 'Saturday'),
+        ('S', 'Sunday'),
+    ]
+    days = models.CharField(max_length=50, choice=DAYS_OF_WEEK)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
 
     class Meta:
         verbose_name = _("schedule")
